f6cc3bf5ac43ceeb3374d80730ca24fba6926b48
Dallin Romney <dallinromney@gmail.com>
2023-08-12 09:44:54 -0400

    0.1.16 minor ismobile fix

--- src/examples/ExampleGearSet.tsx
+++ src/examples/ExampleGearSet.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { ExampleGears, GearSet } from '@dromney/gear-gen';
-import '../styles/examples.css'
 import SpinningOrMouseGearSetViewer from '../components/SpinningOrMouseGearSetViewer';
+import '../styles/examples.css'
 
-function ExampleGearSet({ isMobile = false }: { isMobile?: boolean }) {
+function ExampleGearSet({ spin = false }: { spin?: boolean }) {
     const [gearSet, setGearSet] = useState<GearSet>()
     useEffect(() => {
         setGearSet(new GearSet(ExampleGears()))
@@ -11,7 +11,7 @@ function ExampleGearSet({ isMobile = false }: { isMobile?: boolean }) {
     if (!gearSet) return null
     return (
         <div>
-            <SpinningOrMouseGearSetViewer spin={isMobile} gearSet={gearSet} showGrid={true} padding={3} rpm={20} />
+            <SpinningOrMouseGearSetViewer spin={spin} gearSet={gearSet} showGrid={true} padding={3} rpm={20} />
             <div className="gear-example-download-buttons">
                 <button className="gear-example-download-button" onClick={() => { gearSet.downloadAllSVGs(50) }}>Download all</button>
                 <button className="gear-example-download-button" onClick={() => { gearSet.gears[0].downloadSVG() }}>Download first</button>
